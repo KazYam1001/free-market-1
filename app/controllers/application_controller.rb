5a9a0fda2dea5413619bd9fb6afd9c0b983dfe77
masayaomoto <masaya.omoto@di-v.co.jp>
2020-04-30 13:22:57 +0900

    redirectpath

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
   def production?
     Rails.env.production?
   end
